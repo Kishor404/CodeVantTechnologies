ad61e1eeaf52c181f851afee6fba23ffb6707327
ZX 404 <149653325+kishor404@users.noreply.github.com>
2024-09-29 14:26:48 +0530

    Navbar-Responsive

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,29 +1,55 @@
-import './Components.css'
+import './Components.css';
+import Menu from '../Assets/Icons/Hamburger.svg';
+import Logo from '../Assets/Icons/Hamburger.svg';
+import { useState } from 'react';
 
-const Navbar=()=>{
-    return(
-    <>
-        <section>
-            <div className="Navbar-Cont">
-                <div className="Navbar-L">
-                    <div>
-                        <p>404 Labs</p>
+const Navbar = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
+    return (
+        <>
+            <section>
+                <div className="Navbar-Cont">
+                    <div className="Navbar-L">
+                        <div>
+                            <p>404 Labs</p>
+                        </div>
                     </div>
-                </div>
-                <div className="Navbar-R">
-                    <div>
-                        <a href="#Home">Home</a>
-                        <a href="#About">About</a>
-                        <a href="#Services">Services</a>
-                        <a href="#Projects">Projects</a>
-                        <a href="#Members">Members</a>
-                        <a href="#Contact">Contact</a>
+                    <div className="Navbar-R">
+                        <div>
+                            <a href="#Home">Home</a>
+                            <a href="#About">About</a>
+                            <a href="#Services">Services</a>
+                            <a href="#Projects">Projects</a>
+                            <a href="#Members">Members</a>
+                            <a href="#Contact">Contact</a>
+                        </div>
                     </div>
+                    {/* Responsive part */}
+                    <div className={`Navbar-R-Res ${isNavOpen ? 'open' : ''}`}>
+                        <div>
+                            <a href="#Home">Home</a>
+                            <a href="#About">About</a>
+                            <a href="#Services">Services</a>
+                            <a href="#Projects">Projects</a>
+                            <a href="#Members">Members</a>
+                            <a href="#Contact">Contact</a>
+                        </div>
+                        <div className="Navbar-R-Res-Logo">
+                            <img src={Logo} alt="Logo" />
+                        </div>
+                    </div>
+                    <a className="Navbar-Icon" onClick={toggleNav}>
+                        <img src={Menu} alt="Menu" />
+                    </a>
                 </div>
-            </div>
-        </section>
-    </>
-    )
-}
+            </section>
+        </>
+    );
+};
 
 export default Navbar;
