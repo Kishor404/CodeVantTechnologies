4d8cc6bd0f7781e01d6a068a33b871d1060fd404
ZX 404 <149653325+kishor404@users.noreply.github.com>
2024-09-30 14:38:06 +0530

    Content Updated

--- frontend/src/Pages/Services.js
+++ frontend/src/Pages/Services.js
@@ -1,6 +1,10 @@
 import './Styles/Services.css'
 
 import IOTImg from '../Assets/Images/Services/iot.png'
+import DAImg from '../Assets/Images/Services/Data-Analysis.png'
+import AppImg from '../Assets/Images/Services/App.png'
+import WebImg from '../Assets/Images/Services/Web.jpg'
+
 
 const Services=()=>{
     return(
@@ -11,7 +15,7 @@ const Services=()=>{
                 <div className="Services-Card-All">
                     <div className="Services-Card">
                         <div className='Services-Img'>
-                            <img src={IOTImg} alt="IOT"/>
+                            <img src={IOTImg} alt="IOT Projects"/>
                         </div>
                         <div className="Services-Card-In">
                             <div className="Services-Card-Cont">
@@ -23,11 +27,11 @@ const Services=()=>{
                     </div>
                     <div className="Services-Card">
                         <div className='Services-Img'>
-                            <img src={IOTImg} alt="IOT"/>
+                            <img src={DAImg} alt="Data Analysis Projects"/>
                         </div>
                         <div className="Services-Card-In">
                             <div className="Services-Card-Cont">
-                                <p className="Services-Card-Title">IOT Projects</p>
+                                <p className="Services-Card-Title">Data Analysis Projects</p>
                                 <p className="Services-Card-Text">Sunt aliqua cupidatat ipsum eu elit labore minim ipsum do. Ex sunt ad commodo ea est. <br/>Cillum proident velit labore aliqua ut sunt. Eu exercitation aliquip id occaecat duis est minim aliquip eiusmod anim excepteur id.</p>
                             </div>
                         </div>
@@ -35,11 +39,11 @@ const Services=()=>{
                     </div>
                     <div className="Services-Card">
                         <div className='Services-Img'>
-                            <img src={IOTImg} alt="IOT"/>
+                            <img src={AppImg} alt="App Development"/>
                         </div>
                         <div className="Services-Card-In">
                             <div className="Services-Card-Cont">
-                                <p className="Services-Card-Title">IOT Projects</p>
+                                <p className="Services-Card-Title">App Development</p>
                                 <p className="Services-Card-Text">Sunt aliqua cupidatat ipsum eu elit labore minim ipsum do. Ex sunt ad commodo ea est. <br/>Cillum proident velit labore aliqua ut sunt. Eu exercitation aliquip id occaecat duis est minim aliquip eiusmod anim excepteur id.</p>
                             </div>
                         </div>
@@ -47,11 +51,11 @@ const Services=()=>{
                     </div>
                     <div className="Services-Card">
                         <div className='Services-Img'>
-                            <img src={IOTImg} alt="IOT"/>
+                            <img src={WebImg} alt="Web Development"/>
                         </div>
                         <div className="Services-Card-In">
                             <div className="Services-Card-Cont">
-                                <p className="Services-Card-Title">IOT Projects</p>
+                                <p className="Services-Card-Title">Website Development</p>
                                 <p className="Services-Card-Text">Sunt aliqua cupidatat ipsum eu elit labore minim ipsum do. Ex sunt ad commodo ea est. <br/>Cillum proident velit labore aliqua ut sunt. Eu exercitation aliquip id occaecat duis est minim aliquip eiusmod anim excepteur id.</p>
                             </div>
                         </div>
