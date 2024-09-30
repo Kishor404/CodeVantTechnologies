9f17f48d5690f4ba11d2b500d93c09d3a54036f6
ZX 404 <149653325+kishor404@users.noreply.github.com>
2024-09-30 15:28:28 +0530

    Members

--- frontend/src/Pages/Contact.js
+++ frontend/src/Pages/Contact.js
@@ -1,9 +1,28 @@
+import './Styles/Contact.css'
 
 const Contact=()=>{
     return(
     <>
         <section id="Contact">
-            <p>Contact</p>
+            <div>
+                <div>
+                    <div>
+
+                    </div>
+                    <p>Instagram</p>
+                    <p>LinkedIn</p>
+                    <p>X</p>
+                </div>
+                <div>
+                    <div>
+                        <form>
+                            <input placeholder="Email ID" type='email' required='true'/>
+                            <input placeholder="Message" type='text'required='true'/>
+                            <button type="Sumbit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </section>
     </>
     )
--- frontend/src/Pages/Members.js
+++ frontend/src/Pages/Members.js
@@ -1,11 +1,51 @@
 import './Styles/Members.css'
 
+import Member1 from "../Assets/Images/Members/Member1.jpg"
+
 const Members=()=>{
     return(
     <>
         <section id="Members">
-            <div>
-                <p>Members</p>
+            <div className="Member-Cont">
+                <p className="MemberTitle">| Members</p>
+                <div className="MemberAll">
+                    <div className="MemberCard">
+                        <div className="MemberImg">
+                            <img src={Member1} alt="Member1"/>
+                        </div>
+                        <div className="MemberText">
+                            <p className="MemberName">John Doe</p>
+                            <p>Software Developer</p>
+                        </div>
+                    </div>
+                    <div className="MemberCard">
+                        <div className="MemberImg">
+                            <img src={Member1} alt="Member1"/>
+                        </div>
+                        <div className="MemberText">
+                            <p className="MemberName">John Doe</p>
+                            <p>Software Developer</p>
+                        </div>
+                    </div>
+                    <div className="MemberCard">
+                        <div className="MemberImg">
+                            <img src={Member1} alt="Member1"/>
+                        </div>
+                        <div className="MemberText">
+                            <p className="MemberName">John Doe</p>
+                            <p>Software Developer</p>
+                        </div>
+                    </div>
+                    <div className="MemberCard">
+                        <div className="MemberImg">
+                            <img src={Member1} alt="Member1"/>
+                        </div>
+                        <div className="MemberText">
+                            <p className="MemberName">John Doe</p>
+                            <p>Software Developer</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </>
